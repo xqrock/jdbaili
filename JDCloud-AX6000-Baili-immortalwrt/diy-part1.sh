a4240e2535e994e5557061c4e1686990009cee37
xqrock <58764635+xqrock@users.noreply.github.com>
2025-08-17 18:06:50 +0800

    Update diy-part1.sh

--- JDCloud-AX6000-Baili-immortalwrt/diy-part1.sh
+++ JDCloud-AX6000-Baili-immortalwrt/diy-part1.sh
@@ -42,5 +42,6 @@ git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
 git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
 merge_package https://github.com/kenzok8/openwrt-packages openwrt-packages/luci-app-adguardhome
 merge_package https://github.com/kenzok8/openwrt-packages openwrt-packages/adguardhome
+merge_package https://github.com/kiddin9/kwrt-packages kwrt-packages/luci-app-turboacc
 merge_package https://github.com/kiddin9/kwrt-packages kwrt-packages/wrtbwmon
 merge_package https://github.com/kiddin9/kwrt-packages kwrt-packages/luci-app-wrtbwmon
