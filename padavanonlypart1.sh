f4325df6e01bb9c94d05314fc04f868d99273490
xqrock <58764635+xqrock@users.noreply.github.com>
2025-09-03 08:34:57 +0800

    Update padavanonlypart1.sh

--- padavanonlypart1.sh
+++ padavanonlypart1.sh
@@ -29,7 +29,7 @@ function drop_package(){
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 git clone https://github.com/nikkinikki-org/OpenWrt-nikki.git package/OpenWrt-nikki
-git clone https://github.com/sirpdboy/luci-app-lucky.git package/lucky
+# git clone https://github.com/sirpdboy/luci-app-lucky.git package/lucky
 git clone https://github.com/sirpdboy/luci-app-advanced.git package/luci-app-advanced
 git clone https://github.com/KFERMercer/luci-app-tcpdump.git package/luci-app-tcpdump
 git clone https://github.com/linkease/istore.git package/istore
