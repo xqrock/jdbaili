d23c0562637cf6fff3e1f62a2ffaffd468350330
xqrock <58764635+xqrock@users.noreply.github.com>
2025-09-03 21:14:21 +0800

    Update diy-part2.sh

--- JDCloud-AX6000-Baili-immortalwrt/diy-part2.sh
+++ JDCloud-AX6000-Baili-immortalwrt/diy-part2.sh
@@ -22,6 +22,7 @@ rm -rf ./feeds/packages/net/mosdns
 # rm -rf ./feeds/luci/applications/luci-app-passwall
 # rm -rf ./feeds/luci/applications/luci-app-passwall2
 rm -rf ./feeds/luci/applications/luci-app-ssr-plus
-rm -rf feeds/packages/lang/golang
-git clone https://github.com/sbwml/packages_lang_golang -b 24.x feeds/packages/lang/golang
-
+# rm -rf feeds/packages/lang/golang
+# git clone https://github.com/sbwml/packages_lang_golang -b 24.x feeds/packages/lang/golang
+cp -af ./feeds/mtk_openwrt_feed/24.10/files/* .
+for file in $(find ./feeds/mtk_openwrt_feed/24.10/patches-base -name "*.patch" | sort); do patch -f -p1 -i ${file}; done
