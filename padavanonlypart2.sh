5dcf91d6259e459eef1ffe0b3946b386cd12db28
xqrock <58764635+xqrock@users.noreply.github.com>
2025-09-29 21:23:43 +0800

    Update padavanonlypart2.sh

--- padavanonlypart2.sh
+++ padavanonlypart2.sh
@@ -24,8 +24,7 @@ rm -rf ./feeds/packages/net/mosdns
 # rm -rf ./feeds/luci/applications/luci-app-passwall2
 rm -rf ./feeds/luci/applications/luci-app-ssr-plus
 rm -rf feeds/packages/lang/golang
-git clone https://github.com/sbwml/packages_lang_golang -b 24.x feeds/packages/lang/golang
-
+git clone https://github.com/sbwml/packages_lang_golang -b 25.x feeds/packages/lang/golang
 
 #移除Shadowsocks组件
 PW_FILE=$(find ./ -maxdepth 3 -type f -wholename "*/luci-app-passwall/Makefile")
