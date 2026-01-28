1aca9aa30b9fc8285a00499f9d9bd749c223f3f9
xqrock <58764635+xqrock@users.noreply.github.com>
2026-01-28 14:14:21 +0800

    Update padavanonlypart2.sh

--- padavanonlypart2.sh
+++ padavanonlypart2.sh
@@ -24,7 +24,7 @@ rm -rf ./feeds/packages/net/mosdns
 # rm -rf ./feeds/luci/applications/luci-app-passwall2
 rm -rf ./feeds/luci/applications/luci-app-ssr-plus
 rm -rf feeds/packages/lang/golang
-git clone https://github.com/sbwml/packages_lang_golang -b 25.x feeds/packages/lang/golang
+git clone https://github.com/sbwml/packages_lang_golang -b 26.x feeds/packages/lang/golang
 
 #修复Rust编译失败
 cd /workdir/openwrt/
